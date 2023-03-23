9c536c6b58e82b22a92e1d38edfa0f31b86b628f
dekan06 <dekan06@gmail.com>
2023-03-23 22:22:21 +0300

    2222

--- liba.js
+++ liba.js
@@ -1,9 +1,5 @@
 //https://dekan006.github.io/visiology/liba.js
-function convertAxisAdaptive(value, max, type) {
-  if (!type) {
-    type = "";
-  }
-  let fixed = 2;
+function convertAxisAdaptive(value, max, type = "", fixed = 2) {
   if (max < 1000) {
     return addSpaceFixed(value, fixed) + " " + type;
   }
@@ -94,4 +90,45 @@ function checkNoData(w) {
   } else {
     $(".table-nodata").remove();
   }
+}
+
+function cssStyleFilterYear(w) {
+  var widget = $("#" + w.general.renderTo);
+
+  //Изменить шрифт вложеных значений
+  widget.find(".rb-filter-list-item-text").attr("style", "font-size: 18px");
+  widget.find(".rb-filter-exclude-container").attr("style", "font-size: 18px");
+
+  //изменить стрелку выпадающего меню
+  widget.find(".rb-filter-header-arrow").css({ color: "#0033FF" });
+  // Изменить крестик выпадающего меню
+  widget.find(".rb-filter-header-close").css({ color: "#0033FF" });
+
+  //скрыть кнопи 'Выбрать отображаемое' и  'снять выделение'
+  widget
+    .find(".rb-filter-selection-buttons-container")
+    .css({ display: "none" });
+  widget.find(".rb-filter-header-arrow").css({ color: "#0033FF" });
+
+  // Высота выпадающего списка
+  widget.find(".rb-filter-body-container .rb-filter-list-container > ul").css({
+    height: "auto",
+    "max-height": "500px",
+  });
+  // Мин Ширина выпадающего списка и ширина выпадающего списка
+  widget
+    .find(".rb-filter-body-container")
+    .css({ "min-width": "310px", width: "310px" });
+
+  // Изменяем цвет контура фильтра
+  widget
+    .find(".rb-filter-header-container")
+    .css({ border: "1px solid #C4CDD6" });
+  // Изменяем скругление контура фильтра
+  widget.find(".rb-filter-header-container").css({ "border-radius": "20px" });
+
+  //скрыть кнопки 'Выбрать отображаемое' и  'снять выделение'
+  $("#widget-" + w.general.renderTo)
+    .find(".rb-filter-selection-buttons-container")
+    .css({ display: "none" });
 }
