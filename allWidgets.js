d3e084dcc5b04470f1155f40263f88d1f5bf1c7d
dekan06 <dekan06@gmail.com>
2023-03-23 22:31:20 +0300

    2231

---
+++ allWidgets.js
@@ -0,0 +1,31 @@
+function convertAxisAdaptive(value, max, type = "", fixed = 2) {
+  if (max < 1000) {
+    return addSpaceFixed(value, fixed) + " " + type;
+  }
+  if (max < 1000000) {
+    return addSpaceFixed(Math.round(value / 10) / 100, fixed) + " тыс " + type;
+  }
+  if (max < 1000000000) {
+    return (
+      addSpaceFixed(Math.round(value / 10000) / 100, fixed) + " млн " + type
+    );
+  }
+  if (max < 1000000000000) {
+    return (
+      addSpaceFixed(Math.round(value / 10000000) / 100, fixed) + " млрд " + type
+    );
+  }
+  return (
+    addSpaceFixed(Math.round(value / 10000000000) / 100, fixed) +
+    " трлн " +
+    type
+  );
+}
+
+function addSpaceFixed(value, fixed) {
+  return value
+    .toFixed(fixed)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, " ")
+    .replace(".", ",");
+}
--- liba.js
+++ liba.js
@@ -1,35 +1,4 @@
 //https://dekan006.github.io/visiology/liba.js
-function convertAxisAdaptive(value, max, type = "", fixed = 2) {
-  if (max < 1000) {
-    return addSpaceFixed(value, fixed) + " " + type;
-  }
-  if (max < 1000000) {
-    return addSpaceFixed(Math.round(value / 10) / 100, fixed) + " тыс " + type;
-  }
-  if (max < 1000000000) {
-    return (
-      addSpaceFixed(Math.round(value / 10000) / 100, fixed) + " млн " + type
-    );
-  }
-  if (max < 1000000000000) {
-    return (
-      addSpaceFixed(Math.round(value / 10000000) / 100, fixed) + " млрд " + type
-    );
-  }
-  return (
-    addSpaceFixed(Math.round(value / 10000000000) / 100, fixed) +
-    " трлн " +
-    type
-  );
-}
-
-function addSpaceFixed(value, fixed) {
-  return value
-    .toFixed(fixed)
-    .toString()
-    .replace(/\B(?=(\d{3})+(?!\d))/g, " ")
-    .replace(".", ",");
-}
 
 function formatTooltipChart(thisData, type) {
   return (
