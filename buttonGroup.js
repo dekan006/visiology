c033aaf02e1c235a519f62794e96b36306e82a53
dekan06 <dekan06@gmail.com>
2023-03-27 14:09:58 +0300

    1409

--- buttonGroup.js
+++ buttonGroup.js
@@ -1,11 +1,11 @@
-const defaultColor = "#60769f"; // цвет текста не выбранного элемента
-const activeColor = "#115dee"; // цвет текста выбранного элемента
-const backgroundColor = "#f5f5f7"; // цвет фона выбранного элемента
+const defaultColor1 = "#60769f"; // цвет текста не выбранного элемента
+const activeColor1 = "#115dee"; // цвет текста выбранного элемента
+const backgroundColor1 = "#f5f5f7"; // цвет фона выбранного элемента
 
 document.head.appendChild(document.createElement("style")).innerHTML = `
     .dx-item-selected {
-        background: ${backgroundColor} !important;
-        color: ${activeColor} !important;
+        background: ${backgroundColor1} !important;
+        color: ${activeColor1} !important;
     }`;
 
 function buttonGroup_cssStyle(w) {
@@ -18,6 +18,6 @@ function buttonGroup_cssStyle(w) {
   });
 
   widgetId.find(".dx-item").css({
-    color: defaultColor,
+    color: defaultColor1,
   });
 }
