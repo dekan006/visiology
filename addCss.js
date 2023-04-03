d0e21ddb115adc03a8744c0203d1082c16ea9bbb
dekan06 <dekan06@gmail.com>
2023-04-04 00:47:59 +0300

    047

--- addCss.js
+++ addCss.js
@@ -10,4 +10,26 @@ document.head.appendChild(document.createElement("style")).innerHTML = `
         width: 1910px;
         height: 780px !important;
         background: rgba(0, 0, 0, 0.7);
+    }
+    .help-tooltip {
+        position: absolute;
+        display: none;
+        padding: 10px;
+        top: 30px;
+        background: white;
+        max-width: 1000px;
+        width: max-content;
+        border: 1px solid "#00000057";
+        border-radius: 5px;
+    }
+    .help-tooltip-text{
+        color: "#334059";
+        font-family: "Roboto";
+        font-size: 18px;
+    }
+    .help-tooltip-right {
+    right: 30px;
+    }
+    .help-tooltip-left {
+    left: 30px;
     }`;
---
+++ helpTooltip.js
@@ -0,0 +1,34 @@
+function helpTooltip_afterRender(w, position, helpId) {
+  const widgetId = $("#widget-" + w.general.renderTo);
+
+  const textTooltip = {
+    "01": "Дашборд отражает сведения о контрактах по 44-ФЗ заключенных в выбранном году по показателям в разрезе кодов ОКПД 2:<br><br><b>Количество контрактов </b>– общее количество контрактов, включенных в реестр контрактов ЕИС<br><br><b>Стоимость ТРУ </b>– общая стоимость товаров, работ и услуг по всем позициям контрактов, включенных в реестр контрактов ЕИС<br><br><b>Товары </b>– общая стоимость товаров по позициям контрактов, включенных в реестр контрактов ЕИС<br><br><b>Товары РФ </b>– общая стоимость товаров российского происхождения (ЕАЭС) по позициям контрактов, включенных в реестр контрактов ЕИС",
+  };
+
+  widgetId.css({
+    "z-index": "999",
+  });
+
+  function createTooltip() {
+    const tooltip = document.createElement("div");
+    tooltip.id = "help-tooltip-" + helpId;
+    tooltip.className = `help-tooltip help-tooltip-${position}`;
+    tooltip.innerHTML =
+      '<span class="help-tooltip-text">' + textTooltip.helpId + "</span>";
+    widgetId.append(tooltip);
+  }
+
+  function showTooltip() {
+    widgetId.find(".help-tooltip").show();
+  }
+
+  function hideTooltip() {
+    widgetId.find(".help-tooltip").hide();
+  }
+  createTooltip();
+
+  widgetId.mouseover(showTooltip);
+  widgetId.mouseout(hideTooltip);
+
+  return w;
+}
