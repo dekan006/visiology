60489ae34e2a2b822f37e27a12d6007d94b7d9ee
dekan06 <dekan06@gmail.com>
2023-04-05 13:28:24 +0300

    1328

--- addCss.js
+++ addCss.js
@@ -1,42 +1,42 @@
 document.head.appendChild(document.createElement("style")).innerHTML = `
-    .va-widget-modal {
+    .lib-va-widget-modal {
         width: 1910px !important;
         top: 240px !important;
         height: 850px !important;
         left: 0 !important;
         z-index: 891!important;
     }
-    .va-widget-modal-pie {
+    .lib-va-widget-modal-pie {
         width: 1912px !important;
         top: 240px !important;
         z-index: 900!important;
         height: 830px !important;
         left: 0 !important;
     }
-    .va-table-modal {
+    .lib-va-table-modal {
         width: 1912px !important;
         z-index: 900 !important;
         top: 240px !important;
         height: 830px !important;
         left: 0 !important;
     }
-    .va-chart-modal {
+    .lib-va-chart-modal {
         width: 1912px !important;
         top: 240px !important;
         z-index: 800!important;
         height: 830px !important;
         left: 0 !important;
     }
-    .modal {
+    .lib-modal {
         position: relative;
         width: 1910px;
         height: 780px !important;
         background: rgba(0, 0, 0, 0.7);
     }
-    .zoom-color{
+    .lib-zoom-color{
         fill: blue;
     }
-    .chart-title-label {
+    .lib-chart-title-label {
         position: absolute; 
         font-family: "Roboto"; 
         padding-left: 10px; 
@@ -45,35 +45,35 @@ document.head.appendChild(document.createElement("style")).innerHTML = `
         color: #334059; 
         z-index: 20;
     }
-    .chart-title-label-toZoom {
+    .lib-chart-title-label-toZoom {
         font-size: 25px; 
         color: #334059
     }
-    .сumulative-total {
+    .lib-сumulative-total {
         right: 20px;
         left: auto !important;
     }
-    .сumulative-total {
+    .lib-сumulative-total {
         right: 75px;
         left: auto !important;
     }
-    .сumulative-total-size {
+    .lib-сumulative-total-size {
         font-size: 22px !important;
     }
 
-    .imgZoom-top {
+    .lib-imgZoom-top {
         cursor: pointer;  
         position: absolute; 
         padding-left: 10px; 
         top: 10px; 
         font-size: 22px;
     }
-    .imgZoom-top-toZoom
+    .lib-imgZoom-top-toZoom
         {font-size: 26px;
         color: blue
     }
     
-    .help-tooltip {
+    .lib-help-tooltip {
         position: absolute;
         display: none;
         padding: 10px;
@@ -84,17 +84,17 @@ document.head.appendChild(document.createElement("style")).innerHTML = `
         border: 1px solid #00000057;
         border-radius: 5px;
     }
-    .help-tooltip-text{
+    .lib-help-tooltip-text{
         color: "#334059";
         font-family: "Roboto";
         font-size: 18px;
     }
-    .help-tooltip-right {
+    .lib-help-tooltip-right {
     right: 30px;
     }
-    .help-tooltip-left {
+    .lib-help-tooltip-left {
     left: 30px;
     }
-    .z-index-1000 {
+    .lib-z-index-1000 {
         z-index: 1000 !important
     }`;
