562e25b34b8a747c7c3e6dc06f9a2dab4fb7fe34
dekan06 <dekan06@gmail.com>
2023-04-05 13:23:07 +0300

    1323

---
+++ addLiba.js
@@ -0,0 +1,4 @@
+const scriptLiba = document.createElement("script");
+scriptLiba.type = "text/javascript";
+scriptLiba.src = "https://dekan006.github.io/visiology/addFiles.js";
+document.querySelector("head").append(scriptLiba);
