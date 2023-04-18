88730cc33802c328af80d12daaf54f9a701e5a30
dekan06 <dekan06@gmail.com>
2023-04-19 01:16:06 +0300

    116

--- chartTop.js
+++ chartTop.js
@@ -1,6 +1,10 @@
 function chartTop_cssStyle(w) {}
 
 function chartTop_beforeRender(w, type) {
+  const labelFontSize = "14px";
+  const labelFontSizeZoom = "22px";
+  const titleFontSize = "16px";
+  const titleFontSizeZoom = "22px";
   // Изменение стилей текста
   w.drilldown.activeAxisLabelStyle.fontFamily = "Roboto";
   w.drilldown.activeDataLabelStyle.fontFamily = "Roboto";
@@ -12,14 +16,71 @@ function chartTop_beforeRender(w, type) {
   w.yAxis.labels.style.fontFamily = "Roboto";
   w.tooltip.style.fontFamily = "Roboto";
 
+  let isZoomed = document
+    .getElementById(w.general.renderTo)
+    .classList.contains("modal");
 
-  // Адаптивные подписи значений, осей - цена/штуки
-  w.xAxis.labels.align = "left";
-  w.xAxis.labels.reserveSpace = true;
+  w.tooltip.style.fontSize = isZoomed ? labelFontSizeZoom : labelFontSize;
+  w.xAxis.labels.style.fontSize = isZoomed ? labelFontSizeZoom : labelFontSize;
+  w.yAxis.labels.style.fontSize = isZoomed ? labelFontSizeZoom : labelFontSize;
+  w.yAxis.title.style.fontSize = isZoomed ? titleFontSizeZoom : titleFontSize;
 
   return w;
 }
 
 function chartTop_afterRender(chart, type) {
+  let tickAmount = 7;
+  let maxVal = chart.yAxis[0].max;
+  let isZoomed = document
+    .getElementById(w.general.renderTo)
+    .classList.contains("modal");
+
+  chart.update({
+    chart: {
+      marginRight: 25,
+    },
+    yAxis: {
+      tickAmount: tickAmount,
+      labels: {
+        formatter: function formatter() {
+          let tick = this.axis.tickPositions;
+          if (this.value === 0) {
+            return 0;
+          }
+          let arr = tick.map((item) =>
+            chartAxisAdaptiveLabel(item, this.axis.max).slice(-2)
+          );
+          let fixed = 2;
+          if (arr.every((elem) => elem.charAt(1) === "0")) {
+            fixed = 1;
+          }
+          if (arr.every((elem) => elem === "00")) {
+            fixed = 0;
+          }
+          return chartAxisAdaptiveLabel(this.value, this.axis.max, fixed);
+        },
+      },
+      title: {
+        text: chartAxisAdaptiveTitle(maxVal, type),
+        align: "high",
+        rotation: 0,
+        offset: 0,
+        x: 10,
+        y: isZoomed ? 40 : 30,
+      },
+    },
+    xAxis: {
+      labels: {
+        align: "left",
+        reserveSpace: true,
+      },
+    },
+    tooltip: {
+      formatter: function () {
+        return chartAxisAdaptiveTitle(this.y, type);
+      },
+    },
+  });
+
   return chart;
 }
