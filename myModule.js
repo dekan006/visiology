cca65b69237fa0f01aeb7c047c447de3c9545b4a
dekan06 <dekan06@gmail.com>
2023-04-20 01:25:38 +0300

    125

--- myModule.js
+++ myModule.js
@@ -1,5 +1,90 @@
 const myModule = {
-  afterRender: function (chart, type, w) {
+  fontFamily: "Roboto",
+  labelFontSize: "14px",
+  labelFontSizeZoom: "22px",
+  titleAxisFontSize: "16px",
+  titleAxisSizeZoom: "22px",
+
+  updateFont: function (w) {
+    w.drilldown.activeAxisLabelStyle.fontFamily = this.fontFamily;
+    w.drilldown.activeDataLabelStyle.fontFamily = this.fontFamily;
+    w.legend.itemStyle.fontFamily = this.fontFamily;
+    w.plotOptions.series.dataLabels.style.fontFamily = this.fontFamily;
+    w.xAxis.title.style.fontFamily = this.fontFamily;
+    w.yAxis.title.style.fontFamily = this.fontFamily;
+    w.xAxis.labels.style.fontFamily = this.fontFamily;
+    w.yAxis.labels.style.fontFamily = this.fontFamily;
+    w.tooltip.style.fontFamily = this.fontFamily;
+
+    const isZoomed = document
+      .getElementById(w.general.renderTo)
+      .classList.contains("modal");
+
+    widgetId.find(".va-widget-header")[0].style.fontSize = isZoomed
+      ? "26px"
+      : "21px";
+    w.legend.itemStyle.fontSize = isZoomed
+      ? this.labelFontSizeZoom
+      : this.labelFontSize;
+    w.tooltip.style.fontSize = isZoomed
+      ? this.labelFontSizeZoom
+      : this.labelFontSize;
+    w.plotOptions.series.dataLabels.style.fontSize = isZoomed
+      ? this.labelFontSizeZoom
+      : this.labelFontSize;
+    w.xAxis.labels.style.fontSize = isZoomed
+      ? this.labelFontSizeZoom
+      : this.labelFontSize;
+    w.xAxis.title.style.fontSize = isZoomed
+      ? this.titleAxisSizeZoom
+      : this.titleAxisFontSize;
+    w.yAxis.labels.style.fontSize = isZoomed
+      ? this.labelFontSizeZoom
+      : this.labelFontSize;
+    w.yAxis.title.style.fontSize = isZoomed
+      ? this.titleAxisSizeZoom
+      : this.titleAxisFontSize;
+
+    if (w.yAxis.stackLabels) {
+      w.yAxis.stackLabels.style.fontSize = isZoomed
+        ? this.labelFontSizeZoom
+        : this.labelFontSize;
+    }
+    return w;
+  },
+  lineWidth: function (w) {
+    // толщина линии графика
+    w.series.map((elem) => {
+      elem.lineWidth = 5;
+    });
+    return w;
+  },
+  sortByMonth: function (w) {
+    w.xAxis.categories = sortByMonth(w.xAxis.categories);
+    function sortByMonth(arr) {
+      const months = [
+        "январь",
+        "февраль",
+        "март",
+        "апрель",
+        "май",
+        "июнь",
+        "июль",
+        "август",
+        "сентябрь",
+        "октябрь",
+        "ноябрь",
+        "декабрь",
+      ];
+      arr.sort((a, b) => months.indexOf(a) - months.indexOf(b));
+      return arr;
+    }
+  },
+  tickAmount: function (w, amount) {
+    w.yAxis.tickAmount = amount || 7; //На сколько значений разбивать ось значений
+    return w;
+  },
+  updateChart: function (chart, type, w) {
     const widgetId = $("#widget-" + w.general.renderTo);
     const isZoomed = document
       .getElementById(w.general.renderTo)
